1a2117f02e667afdd57d3dabd7a5baf1515e846d
Guest User <Guest@Epicodus-3B.local>
2018-07-25 16:02:18 -0700

    functionality 90 pct done

--- lib/creature.rb
+++ lib/creature.rb
@@ -8,16 +8,16 @@ class Creature < ActiveRecord::Base
       @creature = Creature.find_by(room_id: current_room.id)
       if @creature == nil
       else
-        if @creature == 'Grandpa'
+        if @creature.name == 'Grandpa'
           Event.create({:entry => 'Grandpa says: Have you seen my shoe? It looks like this one.'})
           Event.create({:entry => 'Grandpa then points at a tea kettle. He stares at you for a moment longer, and then wanders off.'})
-          @creature.update(room_id => nil)
-        elsif @creature == 'Troll'
+          @creature.update(:room_id => nil)
+        elsif @creature.name == 'Troll'
           Event.create({:entry => 'The Troll, who is still blocking the door, looks up at you with its beady eyes, and then slowly tilts its head to the side. You are not sure if it speaks English.'})
-        elsif @creature == 'Ghost'
+        elsif @creature.name == 'Ghost'
           Event.create({:entry => 'Ghost says: ....oooooo .... woooooo.....  beware of the under-the-bed... wooooooooooo. '})
           Event.create({:entry => 'After another moment of intently staring at you, the Ghost disappears.'})
-          @creature.update(room_id => nil)
+          @creature.update(:room_id => nil)
         end
       end
     end
