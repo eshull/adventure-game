402fde1e8512292dc76139390219056782998030
Eric Shull and Nick Brown <nick@>
2018-07-25 15:09:07 -0700

    add shoulda testing

--- Gemfile
+++ Gemfile
@@ -13,3 +13,4 @@ gem("sinatra-contrib", :require => "sinatra/reloader")
 gem("sinatra-activerecord")
 gem("rake", :require => "rake/rdoctask")
 gem 'bootstrap', '~> 4.1.3'
+gem("shoulda-matchers", '~> 2.0')
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,35 +7,53 @@ require('creature')
 require('play')
 require('room')
 require('event')
+require('exit')
 require('player')
 require('pry')
+require('shoulda-matchers')
+
+
+# require("bundler/setup")
+# Bundler.require(:default, :test)
+# set(:root, Dir.pwd())
+
+# require('capybara/rspec')
+# Capybara.app = Sinatra::Application
+# set(:show_exceptions, false)
+require('./app')
+
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
 
 RSpec.configure do |config|
   config.after(:each) do
 
-    Artifact.all.each do |artifact|
-      artifact.destroy()
-    end
 
-    Creature.all.each do |creature|
-      creature.destroy()
-    end
 
-    Play.all.each do |play|
-      play.destroy()
-    end
+    Play.game
 
-    Room.all.each do |room|
-      room.destroy()
-    end
-
-    Event.all.each do |event|
-      event.destroy()
-    end
-
-    Player.all.each do |player|
-      player.destroy()
-    end
+    # Artifact.all.each do |artifact|
+    #   artifact.destroy()
+    # end
+    #
+    # Creature.all.each do |creature|
+    #   creature.destroy()
+    # end
+    #
+    # Play.all.each do |play|
+    #   play.destroy()
+    # end
+    #
+    # Room.all.each do |room|
+    #   room.destroy()
+    # end
+    #
+    # Event.all.each do |event|
+    #   event.destroy()
+    # end
+    #
+    # Player.all.each do |player|
+    #   player.destroy()
+    # end
 
 
 
